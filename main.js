c20fef0f6b67302435b8da705fcc4a62e9edd241
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-10 16:56:12 +0100

    change image randomly

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ var app = new Vue(
         data: {
             textColor: 'text-danger',
             titleContent: 'ciao',
-            mainImage: 'https://picsum.photos/200/300'
+            mainImage: 'https://picsum.photos/300/300',
+            iddio: 'dio'
         },
         methods: {
             changeColor: function(){
-                
+                if(this.textColor == 'text-danger'){
+                    return this.textColor = 'text-primary'
+                 } 
+                 return this.textColor = 'text-danger'
+            },
+            changeImage: function(){
+                if(this.mainImage == 'https://picsum.photos/300/300'){
+                    return this.mainImage = 'https://picsum.photos/300/300/'
+                }
+                return this.mainImage = 'https://picsum.photos/300/300'
             }
 
         }

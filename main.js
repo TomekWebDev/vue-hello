b3af40d9a3f00d0cb82c871b1acda8fa3babcea6
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-10 15:58:48 +0100

    done

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-
+            textColor: 'text-danger',
+            titleContent: 'ciao',
+            mainImage: 'https://picsum.photos/200/300'
         },
         methods: {
-            
+            changeColor: function(){
+                
+            }
+
         }
     }
-)
+);
+
